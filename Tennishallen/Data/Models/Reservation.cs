43d9c69b129756b604a0915c83a8429a6ebebb38
hilgenkampc <christianhilgenkamp@gmail.com>
2024-04-04 11:26:26 +0200

    Models

---
+++ Tennishallen/Data/Models/Reservation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using Tennishallen.Data.Base;
+using Tennishallen.Data.Models;
+
+namespace Tennishallen.Data.Models;
+
+public class Reservation : IBaseEntity<int>
+{
+    [Key] public int Id { get; set; }
+    public Guid MemberId { get; set; }
+    public User? Member{ get; set; }
+    public Guid CoachId { get; set; }
+    public User? Coach { get; set; }
+    public Guid CourtId { get; set; }
+    public Court? Court { get; set; }
+    public double Price { get; set; }
+    public DateOnly Date { get; set; }
+    public int Hour { get; set; }
+
+    internal static void OnModelCreating(ModelBuilder model)
+    {
+        model.Entity<Reservation>()
+            .HasOne(r => r.Member)
+            .WithMany(u => u.MemberReservations)
+            .HasForeignKey(a => a.MemberId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        model.Entity<Reservation>()
+            .HasOne(r => r.Coach)
+            .WithMany(u => u.CoachReservations)
+            .HasForeignKey(a => a.CoachId)
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}

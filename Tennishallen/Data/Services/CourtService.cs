b1143ffc8a2a22bff608b29a9290a728a4eac6be
leslmosnk <leslmosnk@outlook.com>
2024-04-04 20:42:54 +0200

    court crud

---
+++ Tennishallen/Data/Services/CourtService.cs
@@ -0,0 +1,10 @@
+using Tennishallen.Data.Base;
+using Tennishallen.Data.Models;
+
+namespace Tennishallen.Data.Services;
+
+public class CourtService(ApplicationDbContext context) 
+    : BaseRepository<Court, int>(context)
+{
+    
+}

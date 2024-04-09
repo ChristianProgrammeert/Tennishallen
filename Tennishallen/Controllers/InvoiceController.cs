ee78a7777554272e8fd53af00c421825fb0aa124
leslmosnk <leslmosnk@outlook.com>
2024-04-09 17:42:49 +0200

    invoice fzo

--- Tennishallen/Controllers/InvoiceController.cs
+++ Tennishallen/Controllers/InvoiceController.cs
@@ -11,24 +11,32 @@ using Tennishallen.Services;
 
 namespace Tennishallen.Controllers
 {
-    //[AuthFilter(Group.GroupName.Admin, Group.GroupName.Assistent, Group.GroupName.Dentist)]
+    [AuthFilter(Group.GroupName.Admin, Group.GroupName.Coach, Group.GroupName.Member)]
     public class InvoiceController(ApplicationDbContext context) : Controller
     {
         InvoiceService invoiceService = new(context);
+        AuthService authService = new(context);
 
-        public async Task<IActionResult> IndexAsync()
+        public async Task<IActionResult> Index()
         {
-            var userId = new JwtService(Request).GetUserId();
-            var invoices = await invoiceService.GetAllInvoicesForUserAsync(userId);
-            return View(invoices);
+            var jwt = new JwtService(Request);
+            if (!jwt.ValidateToken()) return RedirectToAction("Login", "Auth");
+            if (jwt.GetUserGroups()!.Contains(Group.GroupName.Admin))
+                return View(await authService.GetAllAsync());
+            return await User(jwt.GetUserId()!.Value);
         }
 
-        //public async Task<IActionResult> View(int id)
-        //{
-        //    var token = HttpContext.Request.Cookies["Token"];
-        //    var userId = new JwtService(token).GetUserId();
-        //    var invoice = await invoiceService.GenerateMonthlyInvoiceAsync(userId, year, month);
-        //    return View(invoice);
-        //}
+        public async Task<IActionResult> User(Guid guid)
+        {
+            ViewBag.user = await authService.GetByIdAsync(guid);
+            return View(await invoiceService.GetUserInvoiceDateTimes(guid));
+        }
+        
+        public async Task<IActionResult> View(Guid guid, DateOnly month)
+        {
+            ViewBag.user = await authService.GetByIdAsync(guid);
+            return View(await invoiceService.GetUserReservationsByMonth(guid, month));
+
+        }
     }
 }

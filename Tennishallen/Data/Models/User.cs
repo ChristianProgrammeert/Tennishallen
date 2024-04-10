3f3c062b04d0818793f6936ef6d1ace3668c3607
hilgenkampc <christianhilgenkamp@gmail.com>
2024-04-10 17:32:18 +0200

    spelling + user groups fix

--- Tennishallen/Data/Models/User.cs
+++ Tennishallen/Data/Models/User.cs
@@ -69,7 +69,7 @@ public class User : IBaseEntity<Guid>
             {
                 Id = 1,
                 UserId = guid,
-                Name = Group.GroupName.Coach,
+                Name = Group.GroupName.Admin,
             });
         string[][] names =
         [
@@ -101,7 +101,7 @@ public class User : IBaseEntity<Guid>
                 {
                     Id = i + 2,
                     UserId = guid,
-                    Name = Group.GroupName.Admin,
+                    Name = Group.GroupName.Coach,
                 });
         }
     }

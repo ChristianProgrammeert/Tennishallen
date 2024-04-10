a0043ad4f6c4a6ba057021303f666d5f8b4b6d7a
leslmosnk <leslmosnk@outlook.com>
2024-04-10 19:36:20 +0200

    + default courts in db

--- Tennishallen/Data/Models/Court.cs
+++ Tennishallen/Data/Models/Court.cs
@@ -12,4 +12,41 @@ public class Court : IBaseEntity<int>
     public string Description { get; set; }
     public double Price { get; set; }
     public List<Reservation> Reservations { get; set; }
+
+    internal static void OnModelCreating(ModelBuilder model)
+    {
+        model.Entity<Court>().HasData(
+            new Court()
+            {
+                Id = 1,
+                Name = "Federer Court",
+                Description = "Dit tennisveld is vernoemd naar Roger Federer, een van de meest succesvolle en elegante tennisspelers aller tijden. Federer Court staat bekend om zijn perfecte onderhoud, soepele baanoppervlak en gracieuze uitstraling, passend bij de speelstijl van de legendarische speler zelf.",
+                Price = 40,
+            });
+        model.Entity<Court>().HasData(
+            new Court()
+            {
+                Id = 2,
+                Name = "Serena Williams Arena",
+                Description = "Dit tennisveld is genoemd naar Serena Williams, een icoon van kracht, vastberadenheid en vrouwelijke dominantie op de tennisbaan. Serena Williams Arena staat bekend om zijn robuuste structuur en uitdagende speelomstandigheden, een eerbetoon aan de onverschrokkenheid van Williams tijdens haar legendarische carrière.",
+                Price = 20,
+            });
+        model.Entity<Court>().HasData(
+            new Court()
+            {
+                Id = 3,
+                Name = "Nadal Court",
+                Description = "Vernoemd naar Rafael Nadal, bekend om zijn ongeëvenaarde vastberadenheid, veerkracht en onverslaanbare prestaties op gravelbanen. Nadal Court biedt een uitdagende ondergrond die spelers dwingt tot uiterste inspanning en doorzettingsvermogen, net zoals Nadal dat altijd heeft laten zien.",
+                Price = 1.50,
+            });
+        model.Entity<Court>().HasData(
+            new Court()
+            {
+                Id = 4,
+                Name = "Navratilova Court",
+                Description =
+                    "Genoemd naar Martina Navratilova, een pionier van de vrouwentenniswereld en een symbool van technische perfectie en ongekende atletische vaardigheid. Navratilova Court staat bekend om zijn vlekkeloze grasoppervlak en bevordert het ontwikkelen van een verfijnde, strategische speelstijl, geïnspireerd door de legendarische speler zelf.",
+                Price = 12.50,
+            });
+    }
 }

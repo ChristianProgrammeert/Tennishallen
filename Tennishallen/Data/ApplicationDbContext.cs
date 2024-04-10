2db4758b56f5b83e39b4720f8bb9b788bc3b17e9
leslmosnk <leslmosnk@outlook.com>
2024-04-10 19:36:20 +0200

    + default courts in db

--- Tennishallen/Data/ApplicationDbContext.cs
+++ Tennishallen/Data/ApplicationDbContext.cs
@@ -15,5 +15,6 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         base.OnModelCreating(modelBuilder);
         User.OnModelCreating(modelBuilder);
         Reservation.OnModelCreating(modelBuilder);  
+        Court.OnModelCreating(modelBuilder);
     }
 }
